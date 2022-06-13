5c3530940930f34f5887469a5d9d62e3a3a69560
sdfy-fly <rozhov25@bk.ru>
2022-06-13 18:51:23 +0300

    second

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import News , Category
 
 # Register your models here.
+@admin.register(News)
+class NewsAdmin():
+    list_display = ('id' , 'title' , 'content' , 'created_at')
+    list_display_link = ('id' , 'title' )
+
+
+@admin.register(Category)
+class CategoryAdmin():
+    list_display = ('id' , 'title' )
+    list_display_link = ('id' , 'title' )
--- news/models.py
+++ news/models.py
@@ -1,3 +1,23 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    content = models.TextField(blank=True , verbose_name='Содержимое')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Дата')
+    category = models.ForeignKey('Category' , on_delete= models.CASCADE)
+
+    def __str__(self) : 
+        return self.title
+
+    class Meta() :
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Категория')
+    
+    def __str__(self) : 
+        return self.title
