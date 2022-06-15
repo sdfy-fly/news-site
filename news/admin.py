2d843c35c98430594e3deb24f778e6124d11549b
sdfy-fly <rozhov25@bk.ru>
2022-06-15 16:00:17 +0300

    4

--- news/admin.py
+++ news/admin.py
@@ -3,12 +3,12 @@ from .models import News , Category
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id' , 'title' , 'content' , 'created_at')
-    list_display_link = ('id' , 'title' )
+    list_display_links  = ('id' , 'title' )
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id' , 'title' )
-    list_display_link = ('id' , 'title' )
+    list_display_links  = ('id' , 'title' )
 
 admin.site.register(News , NewsAdmin)
 admin.site.register(Category , CategoryAdmin)
