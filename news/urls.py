6c3ff80308d45caa7f3c2004550b6b7f3c1599c6
sdfy-fly <rozhov25@bk.ru>
2022-07-22 18:54:21 +0300

    register template

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('category/<int:category_id>/' , GetCategory.as_view() , name='category'),
     path('news/<slug:slug>' , ViewNews.as_view() , name='view_news'),
     path('add-news' , CreateNews.as_view() , name='add_news'),
+    path('register' , register , name='register')
 ]
